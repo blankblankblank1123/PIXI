74ad1604d74e5f152867bbbcdf61d003f4186fda
blankblankblank1123 <166421152+blankblankblank1123@users.noreply.github.com>
2024-04-30 18:32:04 +0000

    Success Change Color

--- javaScript/project.js
+++ javaScript/project.js
@@ -1,9 +1,9 @@
 const app = new PIXI.Application({
-    backgroundColor: ad114b,
+    // When changing background color: change "#" -> "0x" and include no quotation marks.
+    backgroundColor: 0x1099bb,
     resizeTo: window
 })
 
 document.body.appendChild(app.view);
 
-document.body.appendChild(app.canvas);
 
