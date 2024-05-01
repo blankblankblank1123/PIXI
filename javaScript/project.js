0147a55dbd60395ce3651f5f77ab31d31b1c0dae
blankblankblank1123 <166421152+blankblankblank1123@users.noreply.github.com>
2024-05-01 10:43:17 -0500

    Quick Commit

--- javaScript/project.js
+++ javaScript/project.js
@@ -8,6 +8,8 @@ document.body.appendChild(app.view);
 
 const SCALE = 1;
 
+let test = 0;
+
 // Adds an image assigned to a variable. 
 app.loader.add('test', 'images/cat.png')
 app.loader.add('test2', 'images/dog.png')
@@ -19,14 +21,29 @@ app.loader.load((loader, resources) => {
     const cat = new PIXI.Sprite(resources.test.texture);
     const dog = new PIXI.Sprite(resources.test2.texture);
     
+    const cheese = new PIXI.Text('HELLO!', {fontFamily: 'Times New Roman'} )
+
     dog.x = (cat.x + 50) * 4.5
     dog.y = 0
     dog.scale.x = SCALE / 3
     dog.scale.y = SCALE / 3
 
-    app.stage.addChild(cat)
+    cheese.x = (cat.x + 100) * 4.5
+    cheese.scale.x = SCALE 
+    cheese.scale.y = SCALE
 
-    app.stage.addChild(dog)
+    app.stage.addChild(cat);
+    app.stage.addChild(dog);
+    app.stage.addChild(cheese)
 
+
+
+    app.ticker.add((time) => {
+        if (test < 10) {
+            test += 1;
+            console.log(test)
+        }
+
+    })
 })
 
